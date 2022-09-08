322bc3c37694c61e6bfe91b156d338a55feff38c
saintajax <saintajax@gmail.com>
2022-09-08 18:23:21 +0300

    second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createIngrMarkup = function (name) {
+  const itemRef = document.createElement("li");
+  itemRef.classList.add("item");
+  itemRef.textContent = name;
+  return itemRef;
+};
+const ingrListRef = ingredients.map(createIngrMarkup);
+document.querySelector("#ingredients").append(...ingrListRef);
