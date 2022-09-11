e2cd6728d43438a75e1252e2ec6279d7c384daee
saintajax <saintajax@gmail.com>
2022-09-11 18:52:20 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const refs = {
+  form: document.querySelector(".login-form"),
+};
 
+refs.form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email;
+  const password = formElements.password;
+  if (email.value === "" || password.value === "") {
+    return alert("Усі поля повинні бути заповнені");
+  }
+  const userData = {};
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    userData.name = name;
+    userData.value = value;
+  });
+
+  console.log(userData);
+  event.currentTarget.reset();
+  return userData;
+}
