f741fdd8b6274defd80b3ac7dc322dcfac7d7817
saintajax <saintajax@gmail.com>
2022-09-11 19:19:45 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  btn: document.querySelector(".change-color"),
+  body: document.querySelector("body"),
+  span: document.querySelector(".color"),
+};
+
+refs.btn.addEventListener("click", onBtnClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onBtnClick() {
+  const currentColor = getRandomHexColor();
+  refs.body.style.backgroundColor = `${currentColor}`;
+  refs.span.textContent = `${currentColor}`;
 }
