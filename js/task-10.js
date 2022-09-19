ef7248eb207576d4d2117e0a5b36daeb7bcf2d3d
saintajax <saintajax@gmail.com>
2022-09-19 19:45:13 +0300

    10task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const api = {
+  stepSize: 10,
+  boxCounter: 0,
+  refs: {
+    input: document.querySelector('[type="number"]'),
+    createBtn: document.querySelector("[data-create]"),
+    destrBtn: document.querySelector("[data-destroy]"),
+    boxesDiv: document.querySelector("#boxes"),
+  },
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+
+  createBoxes() {
+    let amountNumber = Number(this.refs.input.value);
+    let counter = 0;
+    let boxesMarkup = [];
+    for (var i = this.boxCounter; i < amountNumber + this.boxCounter; i += 1) {
+      boxesMarkup.push(
+        `<div style="width:${30 + this.stepSize * i}px; height:${
+          30 + this.stepSize * i
+        }px; background : ${this.getRandomHexColor()}; outline: 1px solid black;" ></div>`
+      );
+      counter += 1;
+    }
+    this.boxCounter += counter;
+    console.log(this);
+    this.refs.boxesDiv.insertAdjacentHTML("beforeend", boxesMarkup.join(""));
+  },
+
+  destroyBoxes() {
+    this.refs.boxesDiv.innerHTML = "";
+    this.boxCounter = 0;
+  },
+};
+
+api.refs.createBtn.addEventListener("click", api.createBoxes.bind(api));
+
+api.refs.destrBtn.addEventListener("click", api.destroyBoxes.bind(api));
